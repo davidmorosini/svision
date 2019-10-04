76a377416002dab363b3c9c831c6940fd98a35eb
davidmorosini <david.morosinineto@gmail.com>
2019-10-04 14:02:09 -0300

    incluindo margem de detecção

--- server/yolov3/image_detect.py
+++ server/yolov3/image_detect.py
@@ -6,7 +6,7 @@ import numpy as np
 import cv2
 
 
-def yolo_detect(yolo, model, img):
+def yolo_detect(yolo, model, img, margin=0):
   
     draw = ImageDraw.Draw(img)
     boxes, shape, class_out, class_name = yolo.get_boxes_detected(img)
@@ -24,7 +24,13 @@ def yolo_detect(yolo, model, img):
             aux = [floor(n) for n in b]
             #cv2.rectangle(im_cv2,(aux[1],aux[0]),(aux[3],aux[2]),(0,255,0),3)
             
-            im_aux = im_cv2[aux[0]:aux[2], aux[1]:aux[3]]
+            xi_ = ((aux[0] - margin) if (aux[0] - margin) >= 0 else 0)
+            yi_ = ((aux[2] - margin) if (aux[2] - margin) >= 0 else 0)
+            xf_ = ((aux[1] + margin) if (aux[1] + margin) >= im_cv2.shape[0] else im_cv2.shape[0])
+            yf_ = ((aux[3] + margin) if (aux[3] + margin) >= im_cv2.shape[1] else im_cv2.shape[1])
+
+            #im_aux = im_cv2[aux[0]:aux[2], aux[1]:aux[3]]
+            im_aux = im_cv2[xi_:yi_, xf_:yf_]
             im_aux_ = cv2.resize(im_aux, (75, 75), interpolation = cv2.INTER_AREA)
             
             res = model.predict([[im_aux_]])
