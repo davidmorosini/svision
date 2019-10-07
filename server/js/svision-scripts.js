0bd060b63ed5f9419250307e7609bfbf6e6b54c3
davidmorosini <david.morosinineto@gmail.com>
2019-10-06 22:05:02 -0300

    fix bugs and add start and att function

---
+++ server/js/svision-scripts.js
@@ -0,0 +1,75 @@
+
+var start_server = false;
+
+var epi = 0;
+var no_epi = 0;
+var person = 0;
+
+function set_display_values(values){
+
+  check = false
+  if(values['webserver']){
+    check = true
+  }
+  client_c = "not-connected"
+  if(values['client']){
+    client_c = "connected"
+  }
+  
+  document.getElementById('client_con').innerText = client_c
+  document.getElementById('myonoffswitch').checked = check
+  document.getElementById('noepi_count').innerText = values['no_epi'];
+  document.getElementById('epi_count').innerText = values['epi'];
+  document.getElementById('person_count').innerText = values['person'];
+}
+
+function att_status(){
+  $.ajax({
+    type:'get',
+    url:'att',
+    cache:false,
+    async:'asynchronous',
+    dataType:'json',
+    success: function(data) {
+      set_display_values(data);
+    },
+    error: function(request, status, error) {
+      set_display_values({
+        "webserver":False,
+        "client":False,
+        "person":0,
+        "epi":0,
+        "no_epi":0
+      })
+    }
+  });
+}
+
+function start_webpage(){
+  att_status();
+  var variavel = setInterval(function() {
+    att_status();
+  }, 4000);
+}
+
+function toggle_service(){
+  start_server = !start_server;
+
+    console.log(start_server)
+    //start_bot/ or test/
+    var req = '/service';
+    
+    $.ajax({
+        type:'get',
+        url:req,
+        cache:false,
+        async:'asynchronous',
+        dataType:'json',
+        success: function(data) {
+          set_display_values(data);
+        },
+        error: function(request, status, error) {
+          console.log("Error: " + error)
+        }
+     });
+}
